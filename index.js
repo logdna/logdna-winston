070a4c238f3662cccd31edc87af673d7d7738e5d
Samir Musali <samir.musali@logdna.com>
2019-09-16 14:41:53 -0400

    Adding UserAgent

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+// External Libraries
 const Transport = require('winston-transport');
 const Logger = require('logdna').Logger;
 const stringify = require('json-stringify-safe');
 
+// Constants
 const DEFAULT_LEVEL = 'debug';
 const DEFAULT_NAME = 'LogDNA';
+
 /*
  *  Support for Winston Transport
  */
 module.exports = class LogDNATransport extends Transport {
     constructor(options) {
+        const pkg = require('./package.json');
         super(options);
         this.name = options.name || DEFAULT_NAME;
         this.level = options.level || DEFAULT_LEVEL;
-        this.logger = new Logger(options.key, options);
         this.index_meta = options.index_meta || false;
+        this.logger = new Logger(options.key, Object.assign({}, options, {
+            UserAgent: `${pkg.name}/${pkg.version}`
+        }));
     }
 
     log(info, callback) {
@@ -22,6 +28,7 @@ module.exports = class LogDNATransport extends Transport {
         if (info.error instanceof Error) {
             info.error = info.error.stack || info.error.toString();
         }
+
         if (!info.message) {
             info.message = stringify(info, null, 2, function() { return undefined; });
         }
@@ -32,6 +39,7 @@ module.exports = class LogDNATransport extends Transport {
             , index_meta: typeof info.index_meta === 'boolean' ? index_meta : this.index_meta
             , context: meta || {}
         };
+        
         this.logger.log(message, opts);
         if (callback) { callback(null, true); }
     }
