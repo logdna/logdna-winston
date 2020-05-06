f5ced3fc5ca94462f8ec81b456953c3daa135eec
Emile Fugulin <code@efugulin.com>
2020-05-05 21:26:43 -0400

    Add type definition

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+import { ConstructorOptions } from "logdna"
+import Transport from "winston-transport";
+
+declare class LogDNATransport extends Transport {
+  constructor(options: LogDNATransport.TransportOptions);
+}
+
+declare namespace LogDNATransport {
+  interface TransportOptions extends Transport.TransportStreamOptions, ConstructorOptions {
+    /** The LogDNA API key. */
+    key: string;
+    /** The name of this transport. */
+    name?: string;
+  }
+}
+
+export = LogDNATransport;
